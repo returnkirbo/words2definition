1d0c48dcbc9fa1878a897f1443b7bbb64a5e1fc7
kirbo <107429396+returnkirbo@users.noreply.github.com>
2023-08-18 07:13:42 -0700

    Add files via upload

--- words2definition.py
+++ words2definition.py
@@ -1,12 +1,33 @@
+import requests
+import json
 
-# opening the file in read mode
-my_file = open("c.txt", "r")
-  
-# reading the file
-data = my_file.read()
-  
-# replacing end splitting the text 
-# when newline ('\n') is seen.
+input_file = open("input.txt", "r")
+data = input_file.read()
+ 
 data_into_list = data.split("\n")
-print(data_into_list)
-my_file.close()
+
+# cleans output file
+open("output.txt", "w").close()
+
+for x in range(0, len(data_into_list)):
+
+    new_string = data_into_list[x]
+    string = 'https://api.dictionaryapi.dev/api/v2/entries/en/' + new_string
+
+    jdata = requests.get(url=string)
+    response_jdata = json.loads(jdata.text)
+
+    with open('output.txt', 'a', encoding='utf-8') as json_file:
+        try:
+            # print(response_jdata[0]['meanings'][0]['definitions'][0]['definition'])
+            print("defining word: " + new_string + " & left over words: " + str(len(data_into_list)-x) + " words")
+            output_data = response_jdata[0]['meanings'][0]['definitions'][0]['definition']
+        except KeyError: 
+            json_file.write(new_string + ": " + "couldn't find definition" + "\n\n")
+            continue
+        json_file.write(new_string + ": " + str(output_data) + "\n\n")
+
+    continue
+
+
+input_file.close()
