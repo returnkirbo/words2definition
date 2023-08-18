77b2106030319312a4cf0136dfc36827c7390a6a
kirbo <107429396+returnkirbo@users.noreply.github.com>
2023-08-18 08:19:42 -0700

    Update words2definition.py
    handling ValueError exceptions

--- words2definition.py
+++ words2definition.py
@@ -15,19 +15,20 @@ for x in range(0, len(data_into_list)):
     string = 'https://api.dictionaryapi.dev/api/v2/entries/en/' + new_string
 
     jdata = requests.get(url=string)
-    response_jdata = json.loads(jdata.text)
 
-    with open('output.txt', 'a', encoding='utf-8') as json_file:
-        try:
-            # print(response_jdata[0]['meanings'][0]['definitions'][0]['definition'])
-            print("defining word: " + new_string + " & left over words: " + str(len(data_into_list)-x) + " words")
-            output_data = response_jdata[0]['meanings'][0]['definitions'][0]['definition']
-        except KeyError: 
-            json_file.write(new_string + ": " + "couldn't find definition" + "\n\n")
-            continue
-        json_file.write(new_string + ": " + str(output_data) + "\n\n")
+    try:
+        response_jdata = json.loads(jdata.text)
+        with open('output.txt', 'a', encoding='utf-8') as json_file:
+            try: 
+                print("defining word: " + new_string + " & left over words: " + str(len(data_into_list)-x) + " words")
+                output_data = response_jdata[0]['meanings'][0]['definitions'][0]['definition']
+                json_file.write(new_string + ": " + str(output_data) + "\n\n")
+            except KeyError:
+                json_file.write(new_string + ": " + "couldn't find definition" + "\n\n")
+                continue
+    except: 
+        print("\nerror in defining word: \"" + new_string + "\" -> is there a space in the file?\n")
 
     continue
-
 
 input_file.close()
